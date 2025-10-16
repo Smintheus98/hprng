75f7a2e8b4f427939db4f06100fc1e2c97cb7179
Yannic Kitten <yannic.kitten@outlook.de>
2025-10-16 14:54:54 +0200

    Add tests for philox generator

---
+++ tests/cxx/philox_reference.cpp
@@ -0,0 +1,228 @@
+/* NOTE:
+ * compile with:
+ *      `g++ philox_reference.cpp -o ./philox_reference-<OS>_<arch>.exe -I /path/to/random123/include/`
+ */
+#include <Random123/philox.h>
+#include <iostream>
+#include <fstream>
+
+const int gen_n = 16;
+
+
+int main(int, char **) {
+    uint32_t xmpl_vals32[] = { 0xdeadbeefU, 0xbeadcafeU, 0x12345678U, 0x90abcdefU, 0xdecafbadU, 0xdef94c5aU, 0x44556677U };
+    uint64_t xmpl_vals64[] = { 0xdeadbeef12345678, 0xdecafbadbeadfeed, 0x8877dd99aa33cc55, 0xbeaea921ab02beef, 0x6f5d422512844f22, 0xad2f6ebd38736225, 0x677b7461a6e523ed };
+
+    // Test cases:
+    //      Philox2x32
+    //          arbitrary key & arbitrary ctr & gen gen_n 
+    //          arbitrary key & arbitrary ctr & jump arbitrary & gen gen_n 
+    //      Philox2x64
+    //          arbitrary key & arbitrary ctr & gen gen_n 
+    //          arbitrary key & arbitrary ctr & jump arbitrary & gen gen_n 
+    //      Philox4x32
+    //          arbitrary key & arbitrary ctr & gen gen_n 
+    //          arbitrary key & arbitrary ctr & jump arbitrary & gen gen_n 
+    //          arbitrary key & arbitrary ctr & jump more than ctr1 & gen gen_n 
+    //      Philox4x64
+    //          arbitrary key & arbitrary ctr & gen gen_n 
+    //          arbitrary key & arbitrary ctr & jump arbitrary & gen gen_n 
+
+    { /* suite 1: Philox2x32 */
+        const int N = 2;
+        std::ofstream ofs("groundtruth/philox2x32.dat");
+        using Philox2x32 = r123::Philox2x32;
+        Philox2x32 gen;
+
+        { /* case 1.1 : arbitrary key, arbitrary ctr, gen gen_n */
+            Philox2x32::key_type key = {{xmpl_vals32[0]}};
+            Philox2x32::ctr_type ctr = {{xmpl_vals32[1], xmpl_vals32[2]}};
+
+            ofs << "key=" << key[0]
+                << ";ctr=" << ctr[0] << "," << ctr[1]
+                << ";gen=";
+
+            for (int i = 0; i < gen_n / N; ++i) {
+                Philox2x32::ctr_type rand = gen(ctr, key);
+                for (int j = 0; j < N; ++j)
+                    ofs << rand[j] << ",";
+                ctr.incr();
+            }
+            ofs << std::endl;
+        } { /* case 1.2 : arbitrary key, arbitrary ctr, arbitrary jump, gen gen_n */
+            Philox2x32::key_type key = {{xmpl_vals32[0]}};
+            Philox2x32::ctr_type ctr = {{xmpl_vals32[1], xmpl_vals32[2]}};
+            auto jmp = xmpl_vals32[3] << (N/2);
+
+            ofs << "key=" << key[0]
+                << ";ctr=" << ctr[0] << "," << ctr[1]
+                << ";jump=" << jmp
+                << ";gen=";
+
+            ctr.incr(jmp >> (N/2));
+
+            for (int i = 0; i < gen_n / N; ++i) {
+                Philox2x32::ctr_type rand = gen(ctr, key);
+                for (int j = 0; j < N; ++j)
+                    ofs << rand[j] << ",";
+                ctr.incr();
+            }
+            ofs << std::endl;
+        } 
+        ofs.close();
+    }
+
+    { /* suite 2: Philox2x64 */
+        const int N = 2;
+        std::ofstream ofs("groundtruth/philox2x64.dat");
+        using Philox2x64 = r123::Philox2x64;
+        Philox2x64 gen;
+
+        { /* case 2.1 : arbitrary key, arbitrary ctr, gen gen_n */
+            Philox2x64::key_type key = {{xmpl_vals64[0]}};
+            Philox2x64::ctr_type ctr = {{xmpl_vals64[1], xmpl_vals64[2]}};
+
+            ofs << "key=" << key[0]
+                << ";ctr=" << ctr[0] << "," << ctr[1]
+                << ";gen=";
+
+            for (int i = 0; i < gen_n / N; ++i) {
+                Philox2x64::ctr_type rand = gen(ctr, key);
+                for (int j = 0; j < N; ++j)
+                    ofs << rand[j] << ",";
+                ctr.incr();
+            }
+            ofs << std::endl;
+        } { /* case 2.2 : arbitrary key, arbitrary ctr, arbitrary jump, gen gen_n */
+            Philox2x64::key_type key = {{xmpl_vals64[0]}};
+            Philox2x64::ctr_type ctr = {{xmpl_vals64[1], xmpl_vals64[2]}};
+            auto jmp = xmpl_vals64[3] << (N/2);
+
+            ofs << "key=" << key[0]
+                << ";ctr=" << ctr[0] << "," << ctr[1]
+                << ";jump=" << jmp
+                << ";gen=";
+
+            ctr.incr(jmp >> (N/2));
+
+            for (int i = 0; i < gen_n / N; ++i) {
+                Philox2x64::ctr_type rand = gen(ctr, key);
+                for (int j = 0; j < N; ++j)
+                    ofs << rand[j] << ",";
+                ctr.incr();
+            }
+            ofs << std::endl;
+        } 
+        ofs.close();
+    }
+
+    { /* suite 3: Philox4x32 */
+        const int N = 4;
+        std::ofstream ofs("groundtruth/philox4x32.dat");
+        using Philox4x32 = r123::Philox4x32;
+        Philox4x32 gen;
+
+        { /* case 3.1 : arbitrary key, arbitrary ctr, gen gen_n */
+            Philox4x32::key_type key = {{xmpl_vals32[0], xmpl_vals32[1]}};
+            Philox4x32::ctr_type ctr = {{xmpl_vals32[2], xmpl_vals32[3], xmpl_vals32[4], xmpl_vals32[5]}};
+
+            ofs << "key=" << key[0] << "," << key[1]
+                << ";ctr=" << ctr[0] << "," << ctr[1] << "," << ctr[2] << "," << ctr[3]
+                << ";gen=";
+
+            for (int i = 0; i < gen_n / N; ++i) {
+                Philox4x32::ctr_type rand = gen(ctr, key);
+                for (int j = 0; j < N; ++j)
+                    ofs << rand[j] << ",";
+                ctr.incr();
+            }
+            ofs << std::endl;
+        } { /* case 3.2 : arbitrary key, arbitrary ctr, arbitrary jump, gen gen_n */
+            Philox4x32::key_type key = {{xmpl_vals32[0], xmpl_vals32[1]}};
+            Philox4x32::ctr_type ctr = {{xmpl_vals32[2], xmpl_vals32[3], xmpl_vals32[4], xmpl_vals32[5]}};
+            auto jmp = xmpl_vals32[6] << (N/2);
+
+            ofs << "key=" << key[0] << "," << key[1]
+                << ";ctr=" << ctr[0] << "," << ctr[1] << "," << ctr[2] << "," << ctr[3]
+                << ";jump=" << jmp
+                << ";gen=";
+
+            ctr.incr(jmp >> (N/2));
+
+            for (int i = 0; i < gen_n / N; ++i) {
+                Philox4x32::ctr_type rand = gen(ctr, key);
+                for (int j = 0; j < N; ++j)
+                    ofs << rand[j] << ",";
+                ctr.incr();
+            }
+            ofs << std::endl;
+        } { /* case 3.3 : arbitrary key, arbitrary ctr, arbitrary long jump, gen gen_n */
+            Philox4x32::key_type key = {{xmpl_vals32[0], xmpl_vals32[1]}};
+            Philox4x32::ctr_type ctr = {{xmpl_vals32[2], xmpl_vals32[3], xmpl_vals32[4], xmpl_vals32[5]}};
+            auto jmp = xmpl_vals64[0] << (N/2);
+
+            ofs << "key=" << key[0] << "," << key[1]
+                << ";ctr=" << ctr[0] << "," << ctr[1] << "," << ctr[2] << "," << ctr[3]
+                << ";jump=" << jmp
+                << ";gen=";
+
+            ctr.incr(jmp >> (N/2));
+
+            for (int i = 0; i < gen_n / N; ++i) {
+                Philox4x32::ctr_type rand = gen(ctr, key);
+                for (int j = 0; j < N; ++j)
+                    ofs << rand[j] << ",";
+                ctr.incr();
+            }
+            ofs << std::endl;
+        } 
+        ofs.close();
+    }
+
+    { /* suite 4: Philox4x64 */
+        const int N = 4;
+        std::ofstream ofs("groundtruth/philox4x64.dat");
+        using Philox4x64 = r123::Philox4x64;
+        Philox4x64 gen;
+
+        { /* case 4.1 : arbitrary key, arbitrary ctr, gen gen_n */
+            Philox4x64::key_type key = {{xmpl_vals64[0], xmpl_vals64[1]}};
+            Philox4x64::ctr_type ctr = {{xmpl_vals64[2], xmpl_vals64[3], xmpl_vals64[4], xmpl_vals64[5]}};
+
+            ofs << "key=" << key[0] << "," << key[1]
+                << ";ctr=" << ctr[0] << "," << ctr[1] << "," << ctr[2] << "," << ctr[3]
+                << ";gen=";
+
+            for (int i = 0; i < gen_n / N; ++i) {
+                Philox4x64::ctr_type rand = gen(ctr, key);
+                for (int j = 0; j < N; ++j)
+                    ofs << rand[j] << ",";
+                ctr.incr();
+            }
+            ofs << std::endl;
+        } { /* case 4.2 : arbitrary key, arbitrary ctr, arbitrary jump, gen gen_n */
+            Philox4x64::key_type key = {{xmpl_vals64[0], xmpl_vals64[1]}};
+            Philox4x64::ctr_type ctr = {{xmpl_vals64[2], xmpl_vals64[3], xmpl_vals64[4], xmpl_vals64[5]}};
+            auto jmp = xmpl_vals64[6] << (N/2);
+
+            ofs << "key=" << key[0] << "," << key[1]
+                << ";ctr=" << ctr[0] << "," << ctr[1] << "," << ctr[2] << "," << ctr[3]
+                << ";jump=" << jmp
+                << ";gen=";
+
+            ctr.incr(jmp >> (N/2));
+
+            for (int i = 0; i < gen_n / N; ++i) {
+                Philox4x64::ctr_type rand = gen(ctr, key);
+                for (int j = 0; j < N; ++j)
+                    ofs << rand[j] << ",";
+                ctr.incr();
+            }
+            ofs << std::endl;
+        } 
+        ofs.close();
+    }
+
+
+    return 0;
+}
---
+++ tests/test_philox.nim
@@ -0,0 +1,281 @@
+import std / unittest
+import std / [strutils, strformat, sequtils]
+
+include hprng/philox
+
+proc toarray[N: static[int], U: SomeUnsignedInt](o: openArray[U]): array[N, U] =
+  try:
+    for i in 0..<o.len: # this might crash (by intention) if the input data is longer than expected (thus invalid)
+      result[i] = o[i]
+  except IndexDefect:
+    echo "Error during array conversion. Data too long!"
+    QuitFailure.quit
+
+
+suite "Test of Philox2x32_10 random number generator":
+  setup:
+    type U = uint32
+    const
+      ctrlen = 2
+      keylen = 1
+    let
+      toctrarray = toarray[ctrlen, U]
+      tokeyarray = toarray[keylen, U]
+    var rng = initPhilox2x32_10()
+
+  test "Compare to ground truth (sampled from Random123)":
+    for line in "tests/cxx/groundtruth/philox2x32.dat".lines:
+      var gen_n: int
+      for data in line.split(";"):
+        if data.startswith("ctr="):
+          let values = data[4..^1].split(",").mapIt(it.parseUInt.U).toctrarray
+          rng.counter(values)
+        elif data.startswith("key="):
+          let values = data[4..^1].split(",").mapIt(it.parseUInt.U).tokeyarray
+          rng.key(values)
+        elif data.startswith("jump="):
+          rng.jump(data[5..^1].parseUInt)
+        elif data.startswith("gen="):
+          let numbers = data[4..^1].strip(chars={','}).split(",").mapIt(it.parseUInt.U)
+          for i in 0..<numbers.len:
+            check rng.next() == numbers[i]
+        else:
+          echo "ERROR: Ground truth data format likely invalid!"
+          QuitFailure.quit
+
+
+suite "Test of Philox2x64_10 random number generator":
+  setup:
+    type U = uint64
+    const
+      ctrlen = 2
+      keylen = 1
+    let
+      toctrarray = toarray[ctrlen, U]
+      tokeyarray = toarray[keylen, U]
+    var rng = initPhilox2x64_10()
+
+  test "Compare to ground truth (sampled from Random123)":
+    for line in "tests/cxx/groundtruth/philox2x64.dat".lines:
+      var gen_n: int
+      for data in line.split(";"):
+        if data.startswith("ctr="):
+          let values = data[4..^1].split(",").mapIt(it.parseUInt.U).toctrarray
+          rng.counter(values)
+        elif data.startswith("key="):
+          let values = data[4..^1].split(",").mapIt(it.parseUInt.U).tokeyarray
+          rng.key(values)
+        elif data.startswith("jump="):
+          rng.jump(data[5..^1].parseUInt)
+        elif data.startswith("gen="):
+          let numbers = data[4..^1].strip(chars={','}).split(",").mapIt(it.parseUInt.U)
+          for i in 0..<numbers.len:
+            check rng.next() == numbers[i]
+        else:
+          echo "ERROR: Ground truth data format likely invalid!"
+          QuitFailure.quit
+
+
+suite "Test of Philox4x32_10 random number generator":
+  setup:
+    type U = uint32
+    const
+      ctrlen = 4
+      keylen = 2
+    let
+      toctrarray = toarray[ctrlen, U]
+      tokeyarray = toarray[keylen, U]
+    var rng = initPhilox4x32_10()
+
+  test "Compare to ground truth (sampled from Random123)":
+    for line in "tests/cxx/groundtruth/philox4x32.dat".lines:
+      var gen_n: int
+      for data in line.split(";"):
+        if data.startswith("ctr="):
+          let values = data[4..^1].split(",").mapIt(it.parseUInt.U).toctrarray
+          rng.counter(values)
+        elif data.startswith("key="):
+          let values = data[4..^1].split(",").mapIt(it.parseUInt.U).tokeyarray
+          rng.key(values)
+        elif data.startswith("jump="):
+          rng.jump(data[5..^1].parseUInt)
+        elif data.startswith("gen="):
+          let numbers = data[4..^1].strip(chars={','}).split(",").mapIt(it.parseUInt.U)
+          for i in 0..<numbers.len:
+            check rng.next() == numbers[i]
+        else:
+          echo "ERROR: Ground truth data format likely invalid!"
+          QuitFailure.quit
+
+
+suite "Test of Philox4x64_10 random number generator":
+  setup:
+    type U = uint64
+    const
+      ctrlen = 4
+      keylen = 2
+    let
+      toctrarray = toarray[ctrlen, U]
+      tokeyarray = toarray[keylen, U]
+    var rng = initPhilox4x64_10()
+
+  test "Compare to ground truth (sampled from Random123)":
+    for line in "tests/cxx/groundtruth/philox4x64.dat".lines:
+      var gen_n: int
+      for data in line.split(";"):
+        if data.startswith("ctr="):
+          let values = data[4..^1].split(",").mapIt(it.parseUInt.U).toctrarray
+          rng.counter(values)
+        elif data.startswith("key="):
+          let values = data[4..^1].split(",").mapIt(it.parseUInt.U).tokeyarray
+          rng.key(values)
+        elif data.startswith("jump="):
+          rng.jump(data[5..^1].parseUInt)
+        elif data.startswith("gen="):
+          let numbers = data[4..^1].strip(chars={','}).split(",").mapIt(it.parseUInt.U)
+          for i in 0..<numbers.len:
+            check rng.next() == numbers[i]
+        else:
+          echo "ERROR: Ground truth data format likely invalid!"
+          QuitFailure.quit
+
+
+
+suite "Counter increment edge-cases for Philox4x32_10":
+  setup:
+    type U = uint32
+    const
+      ctrlen = 4
+      keylen = 2
+    let
+      toctrarray = toarray[ctrlen, U]
+      tokeyarray = toarray[keylen, U]
+    var rng = initPhilox4x32_10()
+    rng.key([0.U, 0.U])
+    rng.counter([0.U, 0.U, 0.U, 0.U])
+
+  test "Overflow increment using next()":
+    rng.counter([U.high, 0.U, 0.U, 0.U])
+    for i in 0..<ctrlen:
+      discard rng.next()
+    check rng.counter == [U.high, 0.U, 0.U, 0.U]
+    check rng.output_it == ctrlen
+    discard rng.next()
+    check rng.counter == [0.U, 1.U, 0.U, 0.U]
+    check rng.output_it == 1
+
+  test "Overflow increment using jump()":
+    rng.counter([U.high, 0.U, 0.U, 0.U])
+    rng.jump(ctrlen)
+    check rng.counter == [0.U, 1.U, 0.U, 0.U]
+    check rng.output_it == 0
+
+  test "Full overflow increment using next()":
+    rng.counter([U.high, U.high, U.high, U.high])
+    for i in 0..<ctrlen:
+      discard rng.next()
+    check rng.counter == [U.high, U.high, U.high, U.high]
+    check rng.output_it == ctrlen
+    discard rng.next()
+    check rng.counter == [0.U, 0.U, 0.U, 0.U]
+    check rng.output_it == 1
+
+  test "Full overflow increment using jump()":
+    rng.counter([U.high, U.high, U.high, U.high])
+    rng.jump(ctrlen)
+    check rng.counter == [0.U, 0.U, 0.U, 0.U]
+    check rng.output_it == 0
+
+
+suite "Extensive jump test for Philox4x32_10":
+  setup:
+    type
+      U = uint32
+      U2 = uint64
+    const
+      ctrlen = 4
+      keylen = 2
+    let
+      toctrarray = toarray[ctrlen, U]
+      tokeyarray = toarray[keylen, U]
+    var rng = initPhilox4x32_10()
+    rng.key([0.U, 0.U])
+    rng.counter([0.U, 0.U, 0.U, 0.U])
+
+  test "Jump 1":
+    rng.jump(1)
+    check rng.counter == [0.U, 0.U, 0.U, 0.U]
+    check rng.output_it == 1
+
+  test "Jump ctrlen-1":
+    rng.jump(ctrlen.pred)
+    check rng.counter == [0.U, 0.U, 0.U, 0.U]
+    check rng.output_it == ctrlen.pred.uint8
+
+  test "Jump ctrlen":
+    rng.jump(ctrlen)
+    check rng.counter == [1.U, 0.U, 0.U, 0.U]
+    check rng.output_it == 0
+
+  test "Jump ctrlen+1":
+    rng.jump(ctrlen.succ)
+    check rng.counter == [1.U, 0.U, 0.U, 0.U]
+    check rng.output_it == 1
+
+  test "Jump 2*ctrlen":
+    rng.jump(2*ctrlen)
+    check rng.counter == [2.U, 0.U, 0.U, 0.U]
+    check rng.output_it == 0
+
+  test "Jump 1 from offset 1":
+    rng.offset(1)
+    rng.jump(1)
+    check rng.counter == [0.U, 0.U, 0.U, 0.U]
+    check rng.output_it == 2
+
+  test "Jump ctrlen-1 from offset 1":
+    rng.offset(1)
+    rng.jump(ctrlen.pred)
+    check rng.counter == [1.U, 0.U, 0.U, 0.U]
+    check rng.output_it == 0
+
+  test "Jump ctrlen from offset 1":
+    rng.offset(1)
+    rng.jump(ctrlen)
+    check rng.counter == [1.U, 0.U, 0.U, 0.U]
+    check rng.output_it == 1
+
+  test "Jump ctrlen+1 from offset 1":
+    rng.offset(1)
+    rng.jump(ctrlen.succ)
+    check rng.counter == [1.U, 0.U, 0.U, 0.U]
+    check rng.output_it == 2
+
+  test "Jump 2*ctrlen from offset 1":
+    rng.offset(1)
+    rng.jump(2*ctrlen)
+    check rng.counter == [2.U, 0.U, 0.U, 0.U]
+    check rng.output_it == 1
+
+  test "Jump U.high from 0":
+    rng.jump(U.high)
+    check rng.counter == [U.high div ctrlen, 0.U, 0.U, 0.U]
+    check rng.output_it == ctrlen.pred.uint8
+
+  test "Jump U.high from U.high":
+    rng.counter([U.high, 0.U, 0.U, 0.U])
+    rng.jump(U.high)
+    check rng.counter == [U.high + (U.high div ctrlen), 1.U, 0.U, 0.U]
+    check rng.output_it == ctrlen.pred.uint8
+
+  test "Jump U2.high from 0":
+    rng.jump(U2.high)
+    check rng.counter == [U.high, U.high div ctrlen, 0.U, 0.U]
+    check rng.output_it == ctrlen.pred.uint8
+
+  test "Jump U2.high from U.high":
+    rng.counter([U.high, 0.U, 0.U, 0.U])
+    rng.jump(U2.high)
+    check rng.counter == [U.high - 1, U.high div ctrlen + 1, 0.U, 0.U]
+    check rng.output_it == ctrlen.pred.uint8
+
