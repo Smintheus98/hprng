e72590a47bb9f941f6b02ec90eeda03e62010d61
Yannic Kitten <yannic.kitten@outlook.de>
2025-10-29 15:57:16 +0100

    Complete functionality of LCG; Jump ahead is not very efficient yet (unsolved: type-overflow on multiplication with non-power-of-2 modulus)

--- src/hprng/lcg.nim
+++ src/hprng/lcg.nim
@@ -9,8 +9,13 @@
 ##
 ## hprng - Linear Congruential Generator (LCG) random number generator family
 ##
-## This implementation was based on the Random123 library which can be found at
-## https://random123.com/, respectively https://github.com/DEShawResearch/random123
+## The classic Linear Congruential Generator is formally defined using the following recurrence:
+## .. math::
+##    x_{n+1} = (a * x_n + c) \mod m
+## with parameters a, c and m, also called multiplier, increment and modulus.
+##
+## Starting with an initial seed :math:`x_0`, this formula delivers a new pseudo-random number
+## :math:`x_{n+1}` on each iteration.
 ##
 
 template makeLinearCongruentialGenerator*(
@@ -42,10 +47,45 @@ template makeLinearCongruentialGenerator*(
   # ***** Internal values and procedures *****
   const use_range_trunc {.gensym.} = (bit_range isnot range[0..0])
 
-  proc trunc(x: U): U {.gensym.} =
-    ## Truncate value according to `bit_range`
-    const bit_range_len = bit_range.high - bit_range.low + 1
-    (x shr bit_range.low) and ( (1.U shl bit_range_len) - 1 )
+  when use_range_trunc:
+    const bit_range_len {.gensym.} = bit_range.high - bit_range.low + 1
+    proc trunc(x: U): U {.gensym, inline.} =
+      ## Truncate value according to `bit_range`.
+      (x shr bit_range.low) and ( (1.U shl bit_range_len) - 1 )
+
+  proc modulo_op(x: U): U {.gensym, inline.} =
+    ## Optimized modulo operation.
+    when modulus_is_power_of_2 and modulus == U.sizeof * 8:
+      return x
+    elif modulus_is_power_of_2 and modulus < U.sizeof * 8:
+      return x and ((1.U shl modulus) - 1)
+    else:
+      return x mod modulus
+
+  #[ # There are some issues left, (actual or emulated) 128-bit arithmetic can solve
+  proc modpow(b, e, m: U): U =
+    ## calculate (b ^ e) mod m
+    discard
+
+  proc calc_jump_multiplier[L: static[int]](jmp_wds: array[L, uint]): array[L, U] {.gensym.} =
+    # TODO: produces wrong results, if `multiplier^2` already overflows U
+    for i in 0..<L:
+      result[i] = modpow(multiplier, jmp_wds[i], modulus)
+      #result[i] = 1
+      #for k in 0..<jmp_wds[i]:
+      #  result[i] = modulo_op(result[i] * modulo_op(multiplier))
+
+  proc calc_jump_increment[L: static[int]](jmp_wds, jmp_mult: array[L, uint]): array[L, U] {.gensym.} =
+    when increment == 0:
+      return
+    for i in 0..<L:
+      result[i] = 0
+
+  const
+    jump_widths: array[4, uint] = [2, 4, 8, 16]
+    jump_multiplier = calc_jump_multiplier[4](jump_widths)
+    #jump_increment = calc_jump_increment[4](jump_widths, jump_multiplier)
+  ]#
 
   # ***** Exported procedures *****
   proc state*(rng: var rngTypeName; state: U) {.inject.} =
@@ -77,20 +117,16 @@ template makeLinearCongruentialGenerator*(
 
   proc next*(rng: var rngTypeName): U {.inject.} =
     ## Return next random number.
-    when modulus_is_power_of_2 and modulus == U.sizeof * 8:
-      rng.state = multiplier * rng.state + increment
-    elif modulus_is_power_of_2 and modulus < U.sizeof * 8:
-      rng.state = (multiplier * rng.state + increment) and ((1.U shl modulus) - 1)
-    else:
-      rng.state = (multiplier * rng.state + increment) mod modulus
-
+    rng.state = modulo_op(multiplier * rng.state + increment)
     when use_range_trunc: return trunc(rng.state)
     else:                 return rng.state
 
   proc jump*[P: Positive](rng: var rngTypeName; n: P) {.inject.} =
     ## Jump ahead by `n` values in the current random number stream.
-    ## TODO: add jumping
-    discard
+    ## TODO: Optimize jump ahead. (NOTE: There are some issues left,
+    ##        only (actual or emulated) 128-bit arithmetic can reliably solve!)
+    for i in 0..<n:
+      discard rng.next()
 
   proc init*(rng: var rngTypeName; seed: varargs[U]) {.inject.} =
     rng.seed(seed)
@@ -103,5 +139,3 @@ makeLinearCongruentialGenerator(minstd, uint32, 48271'u32, 0'u32, 2147483647'u32
 makeLinearCongruentialGenerator(rand48, uint64, 25214903917'u64, 11'u64, 48, true)
 makeLinearCongruentialGenerator(rand48r, uint64, 25214903917'u64, 11'u64, 48, true, range[16..47])
 
-if isMainModule:
-  let rng = initMinstd()
